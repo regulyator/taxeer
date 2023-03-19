ff4f7944baff984b083ec4f74f8807de782a132b
regulyator <regulyator777@gmail.com>
2023-03-20 02:20:53 +0400

    modify income command

--- bot/commands/income.go
+++ bot/commands/income.go
@@ -16,10 +16,16 @@ func HandleIncomeCommand(message *botApi.Message, postgresDb *config.PostgresDb)
 		currentUser := service.GetExistUserOrCreate(postgresDb.Database, strconv.FormatInt(message.From.ID, 10), message.Chat.ID)
 		incomeParams := strings.Split(strings.TrimSpace(strings.Replace(message.Text, "/income", "", 1)), ":")
 		incomeValue, err := strconv.ParseFloat(incomeParams[0], 64)
-		if err != nil || len(incomeParams) != 2 || len(incomeParams[1]) == 0 {
+		if err != nil || len(incomeParams) < 2 || len(incomeParams[1]) == 0 {
 			return "Please input correct income value after command:)"
 		}
-		currencyRate, errGettingRate := service.GetCurrencyAtDate(time.Now(), incomeParams[1])
+
+		rateDate, errParseDate := parseDateInput(incomeParams)
+		if errParseDate != nil {
+			return "Ooops, some error! If you use date in income parameters it should be in 'DD-MM-YYYY' format."
+		}
+
+		currencyRate, errGettingRate := service.GetCurrencyAtDate(rateDate, incomeParams[1])
 		if errGettingRate != nil {
 			return fmt.Sprintf("Error getting currency rate for %s Income not saved!:( Please try again later...", incomeParams[1])
 		}
@@ -37,5 +43,17 @@ func HandleIncomeCommand(message *botApi.Message, postgresDb *config.PostgresDb)
 		return fmt.Sprintf("Income %.2f in %s saved!", savedRecord.IncomeValue, savedRecord.IncomeCurrency)
 	} else {
 		return "Please input correct income value after command:)"
+	}
+}
+
+func parseDateInput(incomeParams []string) (time.Time, error) {
+	if len(incomeParams) == 2 {
+		return time.Now(), nil
+	} else {
+		if parsedDate, err := time.Parse(time.DateOnly, incomeParams[2]); err != nil {
+			return time.Now(), err
+		} else {
+			return parsedDate, nil
+		}
 	}
 }
